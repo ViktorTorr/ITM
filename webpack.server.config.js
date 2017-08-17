f4aabd59d6058d4bd207785bead66215be912c07
krystian nowak <knowak@hpe.com>
2017-08-17 15:01:33 +0200

    [FE] - final polishing

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -8,27 +8,12 @@ const routes = require('./api/routes/employeeRoutes');
 const port = process.env.PORT || 4000;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Employee3db');
+mongoose.connect('mongodb://localhost/EmployeeFinaldb');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 routes(app);
-//ROUTES
-
-
-// const router = express.Router();
-//
-// app.get('/', function (req, res) {
-//   res.send('Hello World!')
-// })
-//
-// app.get('/user', function (req, res) {
-//   res.json({user: 'dupa'});
-// })
-//
-// //PREFIX
-// app.use('/api/employees', routes);
 
 //START ON PORT
 app.listen(port);
